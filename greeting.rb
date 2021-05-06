e6d733a9e1e41605ff9598c66f552c12f75ef21c
Linda Ramos <ltramos7@gmail.com>
2021-05-06 17:29:25 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,21 @@
+# def greeting
+#   puts "Hello World"
+# end
+ 
+# def say_greeting_five_times
+#   greeting
+#   greeting
+#   greeting
+#   greeting
+#   greeting
+# end
+
+# say_greeting_five_times
+
+def greeting_a_person(name, language)
+  puts "Hello #{name}. We heard you are a great #{language} programmer. "
+end
+
+greeting_a_person("Maria", "JavaScript")
+#name = "Maria"
+# greeting_a_person(name)
